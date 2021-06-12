e10b38e34c54903c6ccf79da65beeb62c37403a9
Cassidy <cassburden@gmail.com>
2021-06-12 14:04:35 -0500

    Add consequence for missing the beat

--- conductor/conductor.gd
+++ conductor/conductor.gd
@@ -3,15 +3,21 @@ extends AudioStreamPlayer
 # Pretty much all taken from
 # https://github.com/LegionGames/Conductor-Example/blob/master/Scripts/Conductor.gd
 
-var BPM = 140
-var BPS = BPM / 60.0
-var SPB = 60.0 / BPM
-var BARS = 4
+const BPM = 140
+const BPS = BPM / 60.0
+const SPB = 1.0 / BPS
+const BARS = 4
 const COMPENSATE_FRAMES = 2
 const COMPENSATE_HZ = 60.0
+const NUM_QUIET_BEATS = 3
+const BEAT_INPUT_CUTOFF = SPB / 3.0
+
+onready var sfx = get_node("SFX")
 
 var song_cur_pos = 0.0
 var song_cur_beat = 1
+
+var quiet_until_beat = 0
 
 func strsec(secs):
 	var s = str(secs)
@@ -29,6 +35,11 @@ func _process(_delta):
 	song_cur_beat = int(floor(song_cur_pos * BPS))
 	var seconds = int(song_cur_pos)
 	var seconds_total = int(stream.get_length())
+	
+	if you_are_dummy_bad_at_rhythm():
+		volume_db = -10
+	else:
+		volume_db = 0
 
 
 func start_music():
@@ -49,12 +60,20 @@ func play_from_beat(beat, offset):
 	var beats_before_start = offset
 	var measure = beat % BARS
 	
+func you_are_dummy_bad_at_rhythm():
+	return song_cur_beat < quiet_until_beat
 
 func on_time():
 	if !playing:
 		return false
 		
 	var result = closest_beat(1)
+	
+	if result[1] > BEAT_INPUT_CUTOFF:
+		if not you_are_dummy_bad_at_rhythm():
+			sfx.play()
+		quiet_until_beat = song_cur_beat + NUM_QUIET_BEATS
+	
 	return result[1]
 	
 	# TODO(arjun): implement lmao
